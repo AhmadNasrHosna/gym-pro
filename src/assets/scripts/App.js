3610e1c4a25672ebdad4b28451673eda9d07ef73
Ahmed Nasr <ahmedhosna95@gmail.com>
2019-03-22 11:15:53 +0200

    Finished initial header & footer.

--- src/assets/scripts/App.js
+++
@@ -1,7 +0,0 @@
-
-import MobileMenu from './modules/MobileMenu';
-import RevealEffect from './modules/RevealEffect';
-
-
-const mobileMenu = new MobileMenu;
-const revealEffect = new RevealEffect();
